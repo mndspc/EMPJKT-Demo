803af1182e3dcc6bed9135ea60c29c60a3aa5265
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-10-11 10:02:05 +0530

    Push Unit Test Changes

---
+++ EmpJKT/TestDAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDAL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestDAL")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c987594a-fc46-45bd-8694-f18ea5c92abf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmpJKT/TestDAL/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using DEL;
+namespace TestDAL
+{
+    [TestClass]
+    public class EmpMasterDALTest
+    {
+        [TestMethod]
+        public void TestGetById()
+        {
+            //Arrange:This is first step of Unit test. It perform necessary setup for the test.
+            EmpMasterDAL empMasterDAL = new EmpMasterDAL();
+            var empMaster = new EmpMaster();
+            var empMaster1 = new EmpMaster {EmpCode=123,EmpName= "Scott",DateofBirth=DateTime.Parse("1980-02-03"),Email= "scott@gmail.com",DeptCode= 101 };
+
+            //Act:This is a middle step of unit test.It execute unit test.
+            empMaster = empMasterDAL.GetById(123);
+
+            //Assert:This is a last step of unit test. In this step we check or verify results.
+            Assert.AreEqual(empMaster.EmpCode, empMaster1.EmpCode);
+            //Assert.AreSame(empMaster, empMaster1);
+        }
+
+        [TestMethod]
+        public void TestValidateUser()
+        {
+            //Arrange:
+            UserInfoDAL userInfoDAL = new UserInfoDAL();
+            var userInfo = new UserInfo {Email= "admin123@gmail.com",Password= "admin123" };
+
+            //Act:
+            var flag = userInfoDAL.ValidateUser(userInfo);
+
+            //Assert:
+            //Assert.IsTrue(flag);
+            Assert.IsFalse(flag);
+
+       
+        }
+
+    }
+}
